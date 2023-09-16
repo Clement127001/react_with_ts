02eeeaccbfb8487051631393a242f26754e4423a
Clement127001 <babiyonclement18@gmail.com>
2023-09-16 16:09:16 +0530

    add props to the component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import Todos from "./Components/Todos";
 function App() {
   return (
     <div >
-      <Todos items={['Learn React','Learn Typescript']}/>
+      <Todos items={['Learn React','Learn Typescript']}>
+        <h1>React Todos</h1>
+        <p>add your todos and make your progress to the goal</p>
+      </Todos>
     </div>
   );
 }
--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-const Todos:React.FC<{items:string[]}>=(props)=>
+const Todos:React.FC<{items:string[],children:any}>=(props)=>
 {
 return<div>
     <ul>
         {props.items.map(item=><li key={item}>{item}</li>)}
+        {props.children}
     </ul>
 </div>
 }
